2e95008e4f6c7614d83650e4bd6aee4851171caf
Lucia <112613922+luu522@users.noreply.github.com>
2022-09-01 10:22:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,161 @@
+let isHighlighted = true;
+let vLastHighlighted;
+let firstNum = "";
+let operationSymbol = "";
+let secondNum = "";
+let showSecondNum = false;
+
+window.onload = function (vSymbol) {
+  setResult(0);
+  clean(vSymbol);
+};
+
+function setResult(displayVal) {
+  let display = "";
+  display = display + displayVal;
+  document.getElementById("displayid").innerHTML = display.replace(".", ",");
+}
+
+function getResult() {
+  return document.getElementById("displayid").innerHTML;
+}
+
+function add(key) {
+    if (showSecondNum) {
+        setResult(0);
+        showSecondNum = false;
+    }
+    let result = getResult();
+    if (result.length < 10 ||(result.lastIndexOf(",") <= 9 && result.length < 11)) {
+    if (result != "0" || isNaN(key)) setResult(result + key);
+    else setResult(key);
+  }
+}
+
+function clean(vSymbol) {
+  unhighlightOperators(vLastHighlighted);
+  setResult(0);
+}
+
+function equalUnhighlight() {
+  unhighlightOperators(vLastHighlighted);
+}
+
+function changeSign() {
+  let number = "";
+  number = getResult();
+  let negative = "";
+  negative = negative + number.replace(",", ".");
+  setResult(-negative);
+}
+
+function highlightOperators(vSymbol) {
+  unhighlightOperators(vLastHighlighted);
+  if (isHighlighted == true) {
+    isHighlighted = false;
+    switch (vSymbol) {
+      case "multiply":
+        multiply.style.background = "#5b7aa1";
+        vLastHighlighted = vSymbol;
+        break;
+      case "divide":
+        divide.style.background = "#5b7aa1";
+        vLastHighlighted = vSymbol;
+        break;
+      case "sum":
+        sum.style.background = "#5b7aa1";
+        vLastHighlighted = vSymbol;
+        break;
+      case "substract":
+        substract.style.background = "#5b7aa1";
+        vLastHighlighted = vSymbol;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+function unhighlightOperators(vSymbol) {
+  if (isHighlighted == false) {
+    isHighlighted = true;
+    switch (vSymbol) {
+      case "multiply":
+        multiply.style.background = "#013668";
+        break;
+      case "divide":
+        divide.style.background = "#013668";
+        break;
+      case "sum":
+        sum.style.background = "#013668";
+        break;
+      case "substract":
+        substract.style.background = "#013668";
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+function getFirstNum(op){
+    showSecondNum = true;
+    operationSymbol = op;
+    firstNum = getResult();
+    firstNum = firstNum.replace(",",".");
+}
+
+function getSecondNum(){
+    secondNum = getResult();
+    secondNum = secondNum.replace(",",".");
+}
+
+function equalBtn() {
+  switch (operationSymbol) {
+    case "+":
+      result = Number((parseFloat(firstNum) + parseFloat(secondNum)).toFixed(9));
+      break;
+    case "-":
+      result = Number((parseFloat(firstNum) - parseFloat(secondNum)).toFixed(9));
+      break;
+    case "*":
+      result = Number((parseFloat(firstNum) * parseFloat(secondNum)).toFixed(9));
+      break;
+    case "/":
+      result = Number((parseFloat(firstNum) / parseFloat(secondNum)).toFixed(9));
+      break;
+    default:
+      break;
+  }
+  setResult(result);
+  moreThanTenNums();
+  divisionWithZero();
+  operationWithoutSecondNum();
+  firstNum = 0;
+  secondNum = 0;
+}
+
+function moreThanTenNums(){
+    let result = getResult();
+    if (result.length > 10 ) {
+        setResult("ERROR");
+    }
+}
+
+function divisionWithZero(){
+    let result = "";
+    result = getResult();
+    if (result == "Infinity" || result == NaN) {
+      setResult("ERROR");
+    }
+}
+
+function operationWithoutSecondNum(){
+    let secondNumber = "";
+    secondNumber = getSecondNum();
+    let result = "";
+    result = getResult();
+    if (secondNumber == NaN) {
+      setResult("ERROR");
+    }
+}
